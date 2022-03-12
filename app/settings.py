e11e75a6d7e20d48277fe666339a2b05c11515b7
doginnn <diogenesemmanuel@gmail.com>
2022-03-12 20:42:51 -0300

    [UPD] Criando app contato e atualizando projeto.

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contato',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America_Fortaleza'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
