abafb9d0cdc986d7b2037e7606b2bacdc96d278f
doginnn <diogenesemmanuel@gmail.com>
2022-03-14 22:13:03 -0300

    [ADD] Configurando index.html ao projeto.

--- contato/views.py
+++ contato/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    count = User.objects.count()
+    return render(request, 'index.html', {"count": count})
