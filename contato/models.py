337c411334053138a3d5f1aefd791cc51c02760c
doginnn <diogenesemmanuel@gmail.com>
2022-03-15 22:17:26 -0300

    [UPD] Adicionando model de contato e setando no admin.

--- contato/models.py
+++ contato/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=75)
+    titulo = models.CharField(max_length=100)
+    menssagem = models.TextField(max_length=256)
+
+    def __str__(self):
+        return self.titulo, self.email
