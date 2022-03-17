72a939ebea138e5228a9a26da5761094fc17b04a
doginnn <diogenesemmanuel@gmail.com>
2022-03-16 21:53:58 -0300

    [UPD] Atualizando a aplicacao para o form de contato.

--- contato/views.py
+++ contato/views.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from .forms import ContatoForm
 
 def index(request):
     count = User.objects.count()
     return render(request, 'index.html', {"count": count})
+
+
+def contato(request):
+    form = ContatoForm()
+    context = {'form': form}
+    return render(request, 'contato/contato.html', context)
