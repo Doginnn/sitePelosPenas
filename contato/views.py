9959d208f3b4078696558db423e3d77426232353
doginnn <diogenesemmanuel@gmail.com>
2022-03-23 21:39:38 -0300

    [UPD] Implementando form de contato.

--- contato/views.py
+++ contato/views.py
@@ -8,6 +8,11 @@ def index(request):
 
 
 def contato(request):
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'parciais/_formContato.html')
     form = ContatoForm()
     context = {'form': form}
-    return render(request, 'contato/contato.html', context)
+    return render(request, 'parciais/_formContato.html', context)
