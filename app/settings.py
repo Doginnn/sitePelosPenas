b4c39bd4db2b086f224a2c21a9a028fa0c34b7ef
doginnn <diogenesemmanuel@gmail.com>
2022-03-26 19:04:55 -0300

    [UPD] Configurando e-mail.

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# E-mail Information
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER_GMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD_GMAIL')
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+
+# # Twilio SendGrid Email
+# CONTACT_EMAIL = 'contatopelosepenas@gmail.com'
+# ADMIN_EMAILS = ['admin@example.com', ]
+#
+# # Twilio SendGrid
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'contatopelosepenas@gmail.com'
+# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
