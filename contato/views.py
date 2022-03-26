d569eba613e8c926e32fbc3f05b4644da41887d2
doginnn <diogenesemmanuel@gmail.com>
2022-03-26 19:08:02 -0300

    [UPD] Definir action no _formContato.html para enviar os e-mails.

--- contato/views.py
+++ contato/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.shortcuts import render
 from .forms import ContatoForm
 
@@ -10,9 +12,17 @@ def index(request):
 def contato(request):
     if request.method == 'POST':
         form = ContatoForm(request.POST)
+
+        send_mail(
+            ContatoForm.nome,
+            ContatoForm.email,
+            ContatoForm.titulo,
+            ContatoForm.menssagem
+        )
+
         if form.is_valid():
             form.save()
             return render(request, 'parciais/_formContato.html')
     form = ContatoForm()
     context = {'form': form}
-    return render(request, 'parciais/_formContato.html', context)
+    return render(request, 'index.html', context)
