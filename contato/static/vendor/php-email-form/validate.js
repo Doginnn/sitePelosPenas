4fbdc8ad759ed396335d194b2e3043aad1a52bd2
doginnn <diogenesemmanuel@gmail.com>
2022-03-26 19:06:25 -0300

    [UPD] Modificando o validate.js

--- contato/static/vendor/php-email-form/validate.js
+++ contato/static/vendor/php-email-form/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, 'A propriedade de ação do formulário não está definida!')
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
