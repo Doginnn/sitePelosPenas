48cf6a05e562c595b031132bea4ddc1521952128
doginnn <diogenesemmanuel@gmail.com>
2022-07-05 21:42:24 -0300

    [UPD] Implementando novo form de contato.

--- contato/forms.py
+++ contato/forms.py
@@ -1,8 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Contato
 
+class ContactForm(forms.form):
+    nome = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=75)
+    titulo = forms.CharField(max_length=100)
+    menssagem = forms.TextField(widget=forms.Textarea, max_length=2000)
 
-class ContatoForm(ModelForm):
-    class Meta:
-        model = Contato
-        fields = '__all__'
+# class ContatoForm(ModelForm):
+#     class Meta:
+#         model = Contato
+#         fields = '__all__'
